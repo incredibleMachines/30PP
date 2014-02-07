23d47e048e42b1295ef645194180e6622d88b103
chris.piuggi <chris@piuggi.com>
2014-02-07 10:12:33 -0500

    Updating the front end JS to support new datatypes, adding a sceneController for the /views/event/single.ejs page
    The Scene Controller serves to allow the real time manipulation and editing of scenes in the frontend media manager through jquery. The call is loaded into the footer of the page by passing through the current scene object as parsed via the server routes. This allows the frontend to be manipulated with realtime data without a second ajax call to the server facilitating all the data to be loaded at one time. See footer.ejs for SceneController init functionality.

---
+++ ControllerApp/public/js/sceneController.js
@@ -0,0 +1,123 @@
+/* A function to manipulate scenes on the front-end. 
+
+  _scenes = {OBJ} Takes a JSON Object of _scenes directly from scene render.
+  
+  ** See views/includes/footer.ejs  **
+
+*/
+
+function SceneController(_scenes){
+	
+	var currentScene;
+	//console.log(_scenes);
+	
+	//zone_type modifications and ui changes
+	
+	$('select#zone_type').change(function(e){
+		
+		createAssets($(this).val());
+		
+	}); //#End Zone Type Selection options
+	
+	//if a scene title button is pressed in the sidebar
+	//change the scene with javascript
+	$('button.scene-title').click(function(e){
+		
+		//alert($(this).data('scene-id'))
+		var id = $(this).data('scene-id');
+		
+		//associate ID with obj
+		_scenes.forEach(function(scene,i){
+			//we will always get a match here
+			if(scene._id == id ){ 
+				currentScene = scene;
+				//alert(id);
+				//repopulate the content
+				//change form slug
+				console.log(scene)
+				console.log()
+				console.log(currentScene)
+
+				$('#scene-contents	 form').attr('action', '/scenes/'+currentScene._id);
+				//console.log('.scene-title.'+currentScene.slug)
+				//console.log()
+				
+				//console.log($('.scene-view .scene-title'));
+				$('.scene-view .scene-title').each(function(i){
+					$(this).val(currentScene.title);
+					$(this).html(currentScene.title)
+					
+				});
+				
+				$('.scene-view .scene-zone_type').val(currentScene.zone_type);
+				$('.scene-view .scene-text').val(currentScene.text);
+				
+				//setup the correct zones to go with the scene we are viewing
+				createAssets(currentScene.zone_type)
+				
+			
+			}
+		})
+		
+	});
+}
+
+/*
+**
+** createAssets(_val) 
+** _val = string, a preset value determined by the system currently 'zone_0', 'zone_1','zone_2','zone_3' (subject to updates)
+** 
+*/
+
+
+function createAssets(_val){
+	
+		//TO DO Populate all the assets!
+
+		//clone our original asset (store it in the Browsers Memory)
+		var asset = $('article.asset-single-0').clone();
+		
+		//clear all the old assets from the HTML
+		$('article.asset-single').remove();
+		
+		var assetNum;
+		
+		//assign a number value to our zones - note this is currently hard coded and will be updated
+		switch(_val){
+			//This is will have to change once we know what the fucks we need here.
+			case 'zone_0': assetNum = 1; break;
+			case 'zone_1': assetNum = 2; break;
+			case 'zone_2': assetNum = 3; break;
+			case 'zone_3': assetNum = 4; break; 
+		}
+		
+		//clear all the old single assets from the column
+		$('article.asset-single').remove()
+		for(var i = 0; i< assetNum; i++){
+			//copy our model asset for the number of assets we have
+			var newAsset = $(asset).clone();
+			
+			//update the class structure
+			$(newAsset).removeClass('asset-single-0').addClass('asset-single-'+i);
+			
+			$(newAsset).find('select.asset-file').attr('name','assets['+i+'][file]')
+			$(newAsset).find('textarea.asset-caption').attr('name','assets['+i+'][caption]');
+			$(newAsset).find('select.asset-zone').attr('name','assets['+i+'][zone]')
+			//clear out the asset-zone selections
+			$(newAsset).find('select.asset-zone').html('')
+			
+			//plug in our asset data based off the number of total assets
+			//if the asset # matches the zone # make that option be default selected
+			for(var j = 0; j < assetNum; j++){
+				//console.log(j)
+				if(j == i) $(newAsset).find('select.asset-zone').append('<option value="zone_'+j+'" selected>Zone '+(j+1)+'</option>');
+				else $(newAsset).find('select.asset-zone').append('<option value="zone_'+j+'" >Zone '+(j+1)+'</option>');
+			}
+			
+			
+			
+			//append the data into our holder
+			$('#asset-data').append(newAsset);	
+		}
+	
+}
