65391ab295e153d0315af8d733f256490c10061b
petermania <scott@incrediblemachines.net>
2014-02-10 19:30:46 -0500

    Fixed events linking crash
    Linking crash due to human error
    :wq

---
+++ PlayerApp/src/Compositor/Current Frame.cpp
@@ -0,0 +1,9 @@
+//
+//  Current Frame.cpp
+//  PlayerApp
+//
+//  Created by IncredibleMachines on 2/10/14.
+//
+//
+
+#include "Current Frame.h"
---
+++ PlayerApp/src/Compositor/Current Frame.h
@@ -0,0 +1,14 @@
+//
+//  Current Frame.h
+//  PlayerApp
+//
+//  Created by IncredibleMachines on 2/10/14.
+//
+//
+
+#ifndef __PlayerApp__Current_Frame__
+#define __PlayerApp__Current_Frame__
+
+#include <iostream>
+
+#endif /* defined(__PlayerApp__Current_Frame__) */
