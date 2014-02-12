59f35ca066a2fb32c9a12e1615f6be974c4d036b
chris.piuggi <chris@piuggi.com>
2014-02-12 12:00:53 -0500

    Updating sceneController to handle creation of Assets based on Scene DB Information.
    Modifying the core class of createAssets as a means of modifying the asset structure in the events/single.ejs file. This allows a reference point back to the db scene objects as well as logic to determine which scene is currently being focused on. As the scenes are switched the createAssets function now manipulates html to create the appropriate number of scene assets and if the scene has assets logged in our database object it creates the appropriate assets in the front end.

--- ControllerApp/public/js/sceneController.js
+++ ControllerApp/public/js/sceneController.js
@@ -8,7 +8,7 @@
 
 function SceneController(_scenes){
 	
-	var currentScene;
+	var currentScene = _scenes[0];
 	//console.log(_scenes);
 	
 	//zone_type modifications and ui changes
@@ -60,64 +60,94 @@ function SceneController(_scenes){
 		})
 		
 	});
-}
 
-/*
-**
-** createAssets(_val) 
-** _val = string, a preset value determined by the system currently 'zone_0', 'zone_1','zone_2','zone_3' (subject to updates)
-** 
-*/
-
-
-function createAssets(_val){
+	/*
+	**
+	** createAssets(_val) 
+	** _val = string, a preset value determined by the system currently 'zone_0', 'zone_1','zone_2','zone_3' (subject to updates)
+	** 
+	*/
 	
-		//TO DO Populate all the assets!
-
-		//clone our original asset (store it in the Browsers Memory)
-		var asset = $('article.asset-single-0').clone();
+	
+	function createAssets(_val){
 		
-		//clear all the old assets from the HTML
-		$('article.asset-single').remove();
-		
-		var assetNum;
-		
-		//assign a number value to our zones - note this is currently hard coded and will be updated
-		switch(_val){
-			//This is will have to change once we know what the fucks we need here.
-			case '0': assetNum = 1; break;
-			case '1': assetNum = 2; break;
-			case '2': assetNum = 3; break;
-			case '3': assetNum = 4; break; 
-		}
-		
-		//clear all the old single assets from the column
-		$('article.asset-single').remove()
-		for(var i = 0; i< assetNum; i++){
-			//copy our model asset for the number of assets we have
-			var newAsset = $(asset).clone();
+			//TO DO Populate all the assets!
+			console.log(currentScene.assets.length)
+			//clone our original asset (store it in the Browsers Memory)
+			var asset = $('article.asset-single-0').clone();
 			
-			//update the class structure
-			$(newAsset).removeClass('asset-single-0').addClass('asset-single-'+i);
+			//clear all the old assets from the HTML
+			$('article.asset-single').remove();
 			
-			$(newAsset).find('select.asset-file').attr('name','assets['+i+'][file]')
-			$(newAsset).find('textarea.asset-caption').attr('name','assets['+i+'][caption]');
-			$(newAsset).find('select.asset-zone').attr('name','assets['+i+'][zone]')
-			//clear out the asset-zone selections
-			$(newAsset).find('select.asset-zone').html('')
+			var assetNum;
 			
-			//plug in our asset data based off the number of total assets
-			//if the asset # matches the zone # make that option be default selected
-			for(var j = 0; j < assetNum; j++){
-				//console.log(j)
-				if(j == i) $(newAsset).find('select.asset-zone').append('<option value="'+j+'" selected>Zone '+(j+1)+'</option>');
-				else $(newAsset).find('select.asset-zone').append('<option value="'+j+'" >Zone '+(j+1)+'</option>');
+			//assign a number value to our zones - note this is currently hard coded and will be updated
+			switch(_val){
+				//This is will have to change once we know what the fucks we need here.
+				case '0': assetNum = 1; break;
+				case '1': assetNum = 2; break;
+				case '2': assetNum = 3; break;
+				case '3': assetNum = 4; break; 
 			}
+			//console.log(currentScene.assets.length +" "+assetNum)
+			//clear all the old single assets from the column
+			$('article.asset-single').remove();
 			
+			//
 			
-			
-			//append the data into our holder
-			$('#asset-data').append(newAsset);	
-		}
+			for(var i = 0; i< assetNum; i++){
+				//copy our model asset for the number of assets we have
+				var newAsset = $(asset).clone();
+				//console.log(currentScene.assets[i])
+				//console.log(i+" "+currentScene.assets.indexOf(i));
+				//update the class structure
+				$(newAsset).removeClass('asset-single-0').addClass('asset-single-'+i);
+				
+				$(newAsset).find('select.asset-file').attr('name','assets['+i+'][file]')
+				$(newAsset).find('select.asset-file option:selected').removeAttr('selected');
+
+				$(newAsset).find('textarea.asset-caption').attr('name','assets['+i+'][caption]').html('');
+				$(newAsset).find('select.asset-zone').attr('name','assets['+i+'][zone]')
+				//clear out the asset-zone selections
+				$(newAsset).find('select.asset-zone').html('')
+				
+				//plug in our asset data based off the number of total assets
+				//if the asset # matches the zone # make that option be default selected
+				for(var j = 0; j < assetNum; j++){
+					//console.log(j)
+					if(j == i) $(newAsset).find('select.asset-zone').append('<option value="'+j+'" selected>Zone '+(j+1)+'</option>');
+					else $(newAsset).find('select.asset-zone').append('<option value="'+j+'" >Zone '+(j+1)+'</option>');
+				}
+				
+				if(typeof currentScene.assets[i] !== 'undefined'){ //check if we have a corresponding asset
+					console.log(i+" :: "+JSON.stringify(currentScene.assets[i]));
+  					//iterate through the asset
+					Object.keys(currentScene.assets[i]).forEach(function(key){
+						//make sure that our index's match
+						if(parseInt(key)==i){
+							$(newAsset).find('select.asset-file option[value="'+currentScene.assets[i][key].file+'"]').attr('selected','selected');
+							$(newAsset).find('textarea.asset-caption').html(currentScene.assets[i][key].caption);			
 	
+						}
+					})
+/*
+					Object.keys(currentScene.assets[i]).forEach(function(key){
+						$(newAsset).find('select.asset-file option[value="'+currentScene.assets[i][key]+'"]').attr('selected','selected');
+					})
+*/
+					
+					//$(newAsset).find('select.asset-file').val()
+
+				}else{
+					console.log(i+' undefined ')
+				}
+				
+				//append the data into our holder
+				$('#asset-data').append(newAsset);	
+			}
+		
+	}
+
+
 }
+
