b70116eac3bb01eff0f2de741773acd503c38e2f
jmsaavedra <mailbox.jms@gmail.com>
2014-02-14 15:09:04 -0500

    added event types for pause, resume, end, singles

--- PlayerApp/src/Events/EventTypes.h
+++ PlayerApp/src/Events/EventTypes.h
@@ -22,11 +22,15 @@
 
 
 //--- SOCKET SERVER COMMANDS
-#define INIT_CMD    "init"
-#define SALES_CMD   "sales"
-#define INTRO_CMD   "intro"
-#define OUTRO_CMD   "outro"
-#define ERROR_CMD   "error"
+#define INIT_CMD            "init"
+#define SALES_CMD           "sales"
+#define SALES_SING_CMD      "sales-single"
+#define AMBIENT_CMD         "ambient"
+#define AMBIENT_SING_CMD    "ambient-single"
+#define PAUSE_CMD           "pause"
+#define RESUME_CMD          "resume"
+#define END_CMD             "end"
+#define ERROR_CMD           "error"
 
 
 //--- SOCKET SERVER MESSAGES
