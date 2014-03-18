1c0fcaef7775ed5f119f528be8d7c2be0d19c9ca
jmsaavedra <mailbox.jms@gmail.com>
2014-03-18 15:53:09 -0400

    map functionality front and back-end implemented
    both front-end (map.js) to add and remove locations to the map, as well as all form input functionality for adding and removing (sceneController) are implemented

--- ControllerApp/public/js/map.js
+++ ControllerApp/public/js/map.js
@@ -1,97 +1,164 @@
-function InitMapCanvas(_type,_cb){ 
+
+function InitMapCanvas(_type, _locs, _cb){ 
 
 	var type = _type; //single or multi
 	
 	var canvas = document.getElementById("map");
 	var ctx = canvas.getContext("2d");
 	
+	//TODO: make this dynamic
+	canvas.width = 273;
+	canvas.height = 200;
+	
 	var bgImage = new Image();
 	bgImage.src = "/imgs/map.png";
 	
-	var locationChosen = false;
-	var locationPos = {"x":0, "y":0};
+	var locs = _locs;
+	
+	var locDiameter = 8; // size of circles
+	var locColor = "#0000FF";
+	
+	var selectionId; // index for selecting locations
+	var clickType = 1; // 0=delete  ,  1=add location
+	
+	var debugMsg; //holds debug mouse info
 	
 	
-	
-	//  --- draw function to run on any click 
-	//  	if location has not been chosen, set location.
-	function drawClick(canvas, message, mousePos) {
-		
+	//  --- draw locations on load
+	function drawLocations(){
+
 		ctx.drawImage(bgImage, 0, 0);
+		ctx.fillStyle = "#0000FF"
 		
-		if(!locationChosen){
-			locationPos.x = mousePos.x;
-			locationPos.y = mousePos.y;
-			drawDebug(canvas, message);
-			locationChosen = true;
-			//console.log("LOCATION CHOSEN == FALSE")
-		} else if (locationChosen) {
-			locationChosen = false;
-			//console.log("LOCATION CHOSEN == TRUE")
-		}
-		drawLocationMarker();
-	}
-	
-	// -- draw location marker
-	//	  TODO: make this an image of some sort
-	function drawLocationMarker(){
-		if(locationChosen){
-			ctx.fillStyle="#aa14fa";
+		for(var i=0; i<locs.length; i++){
+			ctx.fillStyle = locs[i].color;
+			
+			var x=parseInt(locs[i].x);
+			var y=parseInt(locs[i].y);
+
 			ctx.beginPath();
-			ctx.arc(locationPos.x, locationPos.y, 5, 0, 2*Math.PI);
+			ctx.arc(x, y, 5, 0, 2*Math.PI);
 			ctx.stroke();
 			ctx.fill();
-		} else {
-			ctx.fillStyle = "#ffffff"
-			ctx.fillRect(30,55,220,30);
-			ctx.font = '12pt Calibri';
-			ctx.fillStyle = 'black';
-			ctx.fillText("click to choose location", 60, 75);
+			
+			//draw coordinates as well
+/* 			ctx.font = '12pt Calibri'; */
+/* 			ctx.fillStyle = 'black'; */
+/* 			ctx.fillText(x+","+y, x+5, y+5); */
 		}
 	}
 	
-	
-	// -- draw debug info on the map
-	//    TODO: remove.
-	function drawDebug(canvas, message) {
-		//ctx.clearRect(0, 0, canvas.width, canvas.height);
+	function drawInstruction(){
+		ctx.drawImage(bgImage, 0, 0);
 		ctx.font = '12pt Calibri';
 		ctx.fillStyle = 'black';
-		ctx.fillText(message, 10, 25);
+		ctx.fillText("click to choose "+type+" location", 30, 100);
 	}
+	
+	
+	
+	//--- process any click on the canvas
+	function processClick(mousePos){
+		if(typeof locs === 'undefined') locs = new Array();
+		switch (clickType){
+			case 0:
+				var thisLoc = {"x":locs[selectionId].x, "y":locs[selectionId].y};
+				_cb(thisLoc, clickType);
+				locs.splice(selectionId, 1);
+				//console.log("REMOVE LOC");
+			break;
+			
+			case 1:
+				var thisLoc = {"x":mousePos.x, "y":mousePos.y};
+				_cb(thisLoc, clickType);
+				locs.push({"x":parseInt(mousePos.x), "y":parseInt(mousePos.y), "color":"#0000FF"});
+				//console.log("ADD LOC");
+			break;	
+		}
+		
+		drawLocations();			
+	}
+	
+	
+	//--- process hovering of mouse on canvas
+	function checkHover(mousePos){
+
+		if(typeof locs !== 'undefined'){ 
+			for(var i=0; i<locs.length; i++){
+				if( parseInt(mousePos.x) <= parseInt(locs[i].x)+locDiameter+3 && 
+					parseInt(mousePos.x) >= parseInt(locs[i].x)-locDiameter-3 &&
+					parseInt(mousePos.y) <= parseInt(locs[i].y)+locDiameter+3 && 
+					parseInt(mousePos.y) >= parseInt(locs[i].y)-locDiameter-3 ){
+					
+					clickType = 0;
+					selectionId = i;
+					locs[i].color = "#FF0000";
+					
+					drawLocations();
+					
+					ctx.font = '12pt Calibri';
+					ctx.fillStyle = 'black';
+					ctx.fillText("remove", parseInt(locs[i].x)-24, parseInt(locs[i].y)-8);
+					//console.log("type REMOVE");
+					break;
+
+				} else {
+					clickType = 1;
+					locs[i].color = "#0000FF";
+
+					drawLocations();
+					//console.log("type ADD");
+				}
+			}
+		} 
+		
+		if(locs.length < 1){
+			clickType = 1;
+			//console.log("hover no locs found");
+			drawInstruction();
+		} 
+	}
+
 	
 	// -- get mousePosition function
 	function getMousePos(canvas, evt) {
 		var rect = canvas.getBoundingClientRect();
 		return {
-		  x: evt.clientX - rect.left,
-		  y: evt.clientY - rect.top
+		  x: Math.floor(evt.clientX - rect.left),
+		  y: Math.floor(evt.clientY - rect.top)
 		};
 	}
+	
 	
 	// -- onload of bg image
 	bgImage.onload = function(){
 	
 		ctx.drawImage(bgImage, 0, 0);
 		
-		drawLocationMarker();
+		if(typeof locs !== 'undefined'){ //if we received locations previously stored
+			console.log("numLocs: "+ locs.length);
+			drawLocations();
+			locationChosen = true;	
+		} else {
+			drawInstruction();
+		}
 		
 		// -- mouse click event listener
 		canvas.addEventListener('click', function(evt) {
 			var mousePos = getMousePos(canvas, evt);
-			var message = 'Loc: ' + Math.floor(mousePos.x) + ', ' + Math.floor(mousePos.y);
-			drawClick(canvas, message, mousePos);
-			_cb(mousePos)
+			debugMsg = 'Loc: ' + mousePos.x + ', ' + mousePos.y;
+			processClick(mousePos);
+			//_cb(mousePos, clickType); //clickType 0=delete, 1=add
 			}, false);
 		
-	/*  -- mousemove event listener
+	   // mousemove event listener
 		canvas.addEventListener('mousemove', function(evt) {
 			var mousePos = getMousePos(canvas, evt);
-			var message = 'Mouse: ' + Math.floor(mousePos.x) + ', ' + Math.floor(mousePos.y);
-			drawDebug(canvas, message);
+			debugMsg = 'Loc: ' + mousePos.x + ', ' + mousePos.y;
+			if(typeof locs !== 'undefined') checkHover(mousePos);
 			}, false);
-	*/
-	}
+			
+	} //end bgImage.onLoad
 }
 
 
