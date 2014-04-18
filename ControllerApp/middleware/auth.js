c15b0d2902b2a16fe961398899267791d316ff38
chris.piuggi <chris@piuggi.com>
2014-04-18 18:56:57 -0400

    Storing Changes to push different mods.

--- ControllerApp/middleware/auth.js
+++ ControllerApp/middleware/auth.js
@@ -2,6 +2,10 @@ exports.index =function(_Database){
   return function(req,res,next){
 
     //here we check if a user has access to this page
-    
+    if(req.session.auth){
+
+    }else{
+      res.redirect('/login')
+    }
   }
 }
