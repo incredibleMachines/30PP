187a4fa076a14c937eecbf5fffc2b84334e759c8
chris.piuggi <chris@piuggi.com>
2014-04-21 10:37:27 -0400

    Adding beta support for login and sessioning.
    Currently all done through stored strings in login basic middleware and authentication strategies are added for support.

--- ControllerApp/middleware/auth.js
+++ ControllerApp/middleware/auth.js
@@ -1,9 +1,8 @@
 exports.index =function(_Database){
   return function(req,res,next){
-
     //here we check if a user has access to this page
-    if(req.session.auth){
-
+    if( (req.session.auth == 1 && req.session.name == 'admin') || (req.session.auth == 0 && req.session.name=='user')){
+      next()
     }else{
       res.redirect('/login')
     }
--- ControllerApp/routes/admin.js
+++ ControllerApp/routes/admin.js
@@ -1,7 +1,28 @@
+exports.index = function(req,res){
+
+  res.render('admin/login',{})
+
+}
 exports.login = function(_Database){
   return function(req,res){
     //check the un and pw on the db
     var post = req.body;
+    //console.log(post)
+
+    if( (post.name == 'admin' && post.creds == 'adminpass') || (post.name == 'user' && post.creds == 'userpass') ){
+      console.log('Login Success')
+      console.log(post)
+      req.session.name = post.name
+      req.session.timestamp = new Date()
+      req.session.auth = (post.name == 'admin')? 1:0
+      res.redirect('/')
+    }else{
+      console.log('Login Fail')
+      console.log(post)
+      res.jsonp({login:'here'})
+
+    }
+
 
     //if it matches create a user session
     //otherwise don't log them in
@@ -12,6 +33,11 @@ exports.login = function(_Database){
 
 exports.logout = function(_Database){
   return function(req,res){
+
     //delete the session
+    req.session.destroy()
+    //req.session.destroy //delete a database
+    res.redirect('/')
+
   }
 }
