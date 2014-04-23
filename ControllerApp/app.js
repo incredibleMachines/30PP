5f468cefc7646055972939bfd5196561c375e69e
jmsaavedra <mailbox.jms@gmail.com>
2014-04-23 14:41:19 -0400

    big timeline updates and ffmpeg concat module created
    timeline collection generated (when MAKE is pressed) has been revamped entirely. start_time, concat_slug, concat_queue all added. methods for make and update have been updated. timeline.ejs has been updated to show start_time. Timeline added to header.
    
    ffmpeg concat module has been created, needs to be tested before implementation.

--- ControllerApp/app.js
+++ ControllerApp/app.js
@@ -35,6 +35,7 @@ var Database = require('./modules/DBConnection');
 var Folders = require('./modules/FolderStructure');
 var AfterEffects = require('./modules/AfterEffects');
 var PathFinder = require('./modules/PathFinder')
+var FFmpeg = require('./modules/FFmpeg');
 
 /**
  *	File Checking
@@ -79,8 +80,8 @@ var app = express();
 //allow our local express files to use underscore
 app.locals._ = require('underscore');
 app.locals.utils = require('./modules/Utils');
-app.locals.EVENT_TYPES = ["Default", "Ambient", "Gastronomy", "Fashion"]
-app.locals.SCENE_TYPES = ["Full Immersion","Single Wall Sculpture","Double Wall Sculpture"]
+app.locals.EVENT_TYPES = ["Default", "Ambient", "Gastronomy", "Fashion"];
+app.locals.SCENE_TYPES = ["Full Immersion","Single Wall Sculpture","Double Wall Sculpture"];
 
 app.set('port', process.env.PORT || 3000);
 app.set('title', '30 Park Place Controller');
