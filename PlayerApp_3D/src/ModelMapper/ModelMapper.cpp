5a5231a8ceb849c83469910b7406459383359fc1
petermania <scott@incrediblemachines.net>
2014-05-21 10:06:36 -0400

    Rolling back to the original textures

--- PlayerApp_3D/src/ModelMapper/ModelMapper.cpp
+++ PlayerApp_3D/src/ModelMapper/ModelMapper.cpp
@@ -1782,47 +1782,47 @@ void ModelMapper:: drawCameras() {
                         ofSetColor(255,255,255);
                         texture->bind();
                         
-                        if(bTransitioning==true){
-                            
-                            if(bTransitionStarted==true){
-                                if(ofGetElapsedTimeMillis()-transitionTimer>transitionTime){
-                                    if(i==numCams-1) bTransitionStarted=false;
-                                    ofSetColor(255,255,255,255);
-                                }
-                                int opacity=int(ofMap(ofGetElapsedTimeMillis()-transitionTimer,0,transitionTime,0,255));
-                                if(opacity>255) opacity=255;
-                                ofSetColor(255,255,255,opacity);
-                                fadeFrame->bind();
-                                
-
-                            }
-                            
-                            else if(bTransitionLoading==true){
-                                if(ofGetElapsedTimeMillis()-transitionTimer>loadTime){
-                                    bTransitionFinished=true;
-                                    bTransitionLoading=false;
-                                    
-                                    
-                                    transitionTimer=ofGetElapsedTimeMillis();
-                                    ofSetColor(255,255,255,255);
-                                }
-                                ofSetColor(255,255,255,255);
-                                fadeFrame->bind();
-
-                            }
-                            
-                            else if(bTransitionFinished==true){
-                                if(ofGetElapsedTimeMillis()-transitionTimer>transitionTime){
-                                    bTransitionFinished=false;
-                                    bTransitioning=false;
-                                    ofSetColor(255,255,255,0);
-                                }
-                                ofSetColor(255,255,255,int(ofMap(ofGetElapsedTimeMillis()-transitionTimer,0,transitionTime,255,0)));
-                                
-                                fadeFrame->bind();
-                            }
-                            
-                        }
+//                        if(bTransitioning==true){
+//                            
+//                            if(bTransitionStarted==true){
+//                                if(ofGetElapsedTimeMillis()-transitionTimer>transitionTime){
+//                                    if(i==numCams-1) bTransitionStarted=false;
+//                                    ofSetColor(255,255,255,255);
+//                                }
+//                                int opacity=int(ofMap(ofGetElapsedTimeMillis()-transitionTimer,0,transitionTime,0,255));
+//                                if(opacity>255) opacity=255;
+//                                ofSetColor(255,255,255,opacity);
+//                                fadeFrame->bind();
+//                                
+//
+//                            }
+//                            
+//                            else if(bTransitionLoading==true){
+//                                if(ofGetElapsedTimeMillis()-transitionTimer>loadTime){
+//                                    bTransitionFinished=true;
+//                                    bTransitionLoading=false;
+//                                    
+//                                    
+//                                    transitionTimer=ofGetElapsedTimeMillis();
+//                                    ofSetColor(255,255,255,255);
+//                                }
+//                                ofSetColor(255,255,255,255);
+//                                fadeFrame->bind();
+//
+//                            }
+//                            
+//                            else if(bTransitionFinished==true){
+//                                if(ofGetElapsedTimeMillis()-transitionTimer>transitionTime){
+//                                    bTransitionFinished=false;
+//                                    bTransitioning=false;
+//                                    ofSetColor(255,255,255,0);
+//                                }
+//                                ofSetColor(255,255,255,int(ofMap(ofGetElapsedTimeMillis()-transitionTimer,0,transitionTime,255,0)));
+//                                
+//                                fadeFrame->bind();
+//                            }
+//                            
+//                        }
 
 
 
@@ -1837,9 +1837,9 @@ void ModelMapper:: drawCameras() {
                         
                         cameras[i].mesh[j].draw();
                         texture->unbind();
-                        if(bTransitioning==true){
-                            fadeFrame->unbind();
-                        }
+//                        if(bTransitioning==true){
+//                            fadeFrame->unbind();
+//                        }
 //                        else{
 
 //                        }
